7a512bfb6d62ca94be3bab23d1a6ff4182ec8776
pbortnick <pbortnick@gmail.com>
2018-03-13 23:35:04 -0400

    fixed hover issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './css/App.css';
 import Header from './components/Header';
 import Title from './components/Title';
 import ManagementTeam from './containers/ManagementTeam';
--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -3,11 +3,13 @@ import React from 'react';
 const EmployeeCard = ({ employee }) => (
 
   <div className="EmployeeCard">
-    <img src={employee.photo} alt={employee.title} />
-    <div className="cardSubtitle">
-      <div className="name">{employee.name}</div>
-      <div className="position">{employee.position}</div>
-      <div className="learn-more">LEARN MORE</div>
+    <div className="CardContainer">
+      <img src={employee.photo} alt={employee.title} />
+      <div className="cardSubtitle">
+        <div className="name">{employee.name}</div>
+        <div className="position">{employee.position}</div>
+        <div className="learn-more">LEARN MORE</div>
+      </div>
     </div>
   </div>
 )
